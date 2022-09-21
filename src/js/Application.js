05e5cb47676d6404a5ec5e866cd75a28b11d5d2c
Jayed07 <jayed.ws7@gmail.com>
2022-09-21 17:02:45 +0300

    add bananas to monkeys

--- src/js/Application.js
+++ src/js/Application.js
@@ -13,9 +13,28 @@ export default class Application extends EventEmitter {
     this.banana = "🍌";
     this.emit(Application.events.READY);
   }
+
   setEmojis(emojis) {
     this.emojis = emojis;
+
+    let div = document.getElementById("emojis");
+    div.innerHTML = '';
+
+    let paragraph = document.createElement("p");
+    // paragraph.innerHTML = this.emojis;
+
+    div.appendChild(paragraph);
   }
 
-  addBananas() {}
+  addBananas() {
+    let elementToAdd = this.banana;
+    let newArr = this.emojis.map(function(monkey) {
+      return monkey.concat('', elementToAdd);
+    });
+
+    this.emojis = newArr;
+
+    let paragraph = document.querySelector('p');
+    paragraph.innerHTML = this.emojis;
+  }
 }
